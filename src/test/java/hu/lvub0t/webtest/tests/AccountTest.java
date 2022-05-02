f59a467379f32017126033139ccac0d7d2e9ee48
Attila Láber <atilaber@gmail.com>
2022-05-02 22:26:11 +0200

    Make checkNameChange test use random usernames

--- src/test/java/hu/lvub0t/webtest/tests/AccountTest.java
+++ src/test/java/hu/lvub0t/webtest/tests/AccountTest.java
@@ -3,9 +3,12 @@ package hu.lvub0t.webtest.tests;
 import static org.junit.Assert.assertEquals;
 import static hu.lvub0t.webtest.util.TestUtils.ACCOUNT_PAGE_TITLE;
 import static hu.lvub0t.webtest.util.TestUtils.EMAIL;
+import static hu.lvub0t.webtest.util.TestUtils.NEW_NAMES;
 import static hu.lvub0t.webtest.util.TestUtils.PASSWORD;
 
 import org.junit.Test;
+
+import java.util.Random;
 
 import hu.lvub0t.webtest.pages.AccountPage;
 import hu.lvub0t.webtest.pages.HomePage;
@@ -24,7 +27,11 @@ public class AccountTest extends TestBase {
 
     @Test
     public void checkNameChange() {
-        String newName = "Mrs. Ape";
+
+        Random rand = new Random();
+        int index = rand.nextInt(NEW_NAMES.length);
+
+        String newName = NEW_NAMES[index];
         SigninPage signInPage = new SigninPage(driver);
         HomePage homePage = signInPage.loginValidUser(EMAIL, PASSWORD);
         AccountPage accountPage = homePage.goToAccountPage();
